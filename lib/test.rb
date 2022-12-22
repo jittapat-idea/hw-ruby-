ce4b26147941b01b2bddf106cdff98106dc00515
jittapat <idea26746@gmail.com>
2022-12-22 16:09:33 +0700

    สร้าง def hello

--- lib/test.rb
+++ lib/test.rb
@@ -11,15 +11,27 @@
 #   end
 # arr = [1,2,3,4]
 # puts max_2_sum( arr)
-arr = [10,20,30,40]
-n = 40
-for x in 0..arr.length-1 do
-  for y in  1..arr.length-1 do
-    if arr[x] + arr[y] == n
-      puts 1
-      return true
-      
-    end
-  end
-end
-print "zzz"
+
+
+# arr = [10,20,30,40]
+# n = 40
+# ans = 0
+# # x เเละ y เป็นตำเเหน่งใน array
+# # step 1 .x => 0 
+# #         y => 1 , 2 , 3
+# # step 2 .x => 1 
+# #         y => 2 , 3
+# # step 3 .x => 2
+# #         y => 3
+# # step 4 .x => 3
+         
+# for x in 0..arr.length-1 do
+#   puts " x is #{x}"
+#   for y in x + 1..arr.length-1 do
+#     puts " y is #{y}"
+#     if arr[x] + arr[y] == n
+#       ans = 1
+#       puts ans
+#     end
+#   end
+# end
